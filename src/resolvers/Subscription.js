725bd9a227bf93e25dd36f3f2a114028546fee44
Ryan Chua <ryan.s.chua@gmail.com>
2019-01-09 23:27:02 -0800

    add status type, delete subscription

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,5 +1,9 @@
-function newCarSubscribe(parent, args, context, info) {
-  return context.prisma.$subscribe.car({ mutation_in: ['CREATED'] }).node()
+function newCarSubscribe(_parent, _args, context, _info) {
+  return context.prisma.$subscribe.car({ mutation_in: ['CREATED'] }).node();
+}
+
+function deleteCarSubscribe(_parent, _args, context, _info) {
+  return context.prisma.$subscribe.car({ mutation_in: ['DELETED'] }).node();
 }
 
 const newCar = {
@@ -7,8 +11,16 @@ const newCar = {
   resolve: payload => {
     return payload;
   },
-}
+};
+
+const deletedCar = {
+  subscribe: deleteCarSubscribe,
+  resolve: () => {
+    return { status: 200, message: 'OK' };
+  },
+};
 
 module.exports = {
   newCar,
+  deletedCar,
 }
